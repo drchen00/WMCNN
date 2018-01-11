43c2574b54117946b0f8ca147006ee4b8d250fca
drchen00 <drchen00@gmail.com>
2018-01-12 02:13:45 +0800

    load data

--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python
+import sys
+import numpy as np
+
+def load_data(path):
+    data = np.loadtxt(path,delimiter=',')
+    matrix = data[:,1:]
+    label = data[:,0]
+    return matrix,label
+
+if __name__ == '__main__':
+    matrix,label=load_data('../UCR_TS_Archive_2015/50words/50words_TRAIN')
+    print(matrix.shape) 
+
