8ff17824e3590fa501bbe2452611f71837b3e67e
drchen00 <drc12345>
2018-01-12 21:44:20 +0800

    data_load

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-import sys
 import numpy as np
 
 def load_data(path):
-    data = np.loadtxt(path,delimiter=',')
-    matrix = data[:,1:]
-    label = data[:,0]
-    return matrix,label
+    data = np.loadtxt(path, delimiter=',')
+    rng = np.random.RandomState(12345)
+    ind = np.arange(data.shape[0])
+    rng.shuffle(ind)
+    data = data[ind]
+    matrix = data[:, 1:]
+    label = data[:, 0]
+    return matrix, label
 
 if __name__ == '__main__':
-    matrix,label=load_data('../UCR_TS_Archive_2015/50words/50words_TRAIN')
-    print(matrix.shape) 
-
+    matrix, label = load_data('../UCR_TS_Archive_2015/CBF/CBF_TRAIN')
+    n = matrix.shape[0]
+    print(matrix)
