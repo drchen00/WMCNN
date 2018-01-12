65ca9290ed02e9f03b4bf69c8082b3e24c5d4ad4
drchen00 <drchen00@gmail.com>
2018-01-13 02:18:33 +0800

    slice_data

--- data_preparation.py
+++ data_preparation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import numpy as np
 
-def load_data(path):
+
+def _load_data(path):
     data = np.loadtxt(path, delimiter=',')
     rng = np.random.RandomState(12345)
     ind = np.arange(data.shape[0])
@@ -11,7 +12,27 @@ def load_data(path):
     label = data[:, 0]
     return matrix, label
 
+
+def slice_data(path, length=1024):
+    matrix, label = _load_data(path)
+    ori_num = matrix.shape[0]
+    ori_len = matrix.shape[1]
+    threshold = min(ori_len, length) / 2
+    length = 1
+    while length <= threshold:
+        length <<= 1
+    mul = ori_len - length + 1
+    n = ori_num * mul
+    new_matrix = np.zeros((n, length))
+    new_label = np.zeros(n)
+    for i in range(ori_num):
+        for j in range(mul):
+            new_matrix[i * mul + j, :] = matrix[i, j:j + length]
+            new_label[i * mul + j] = np.int_(label[i])
+    return new_matrix, new_label
+
+
 if __name__ == '__main__':
-    matrix, label = load_data('../UCR_TS_Archive_2015/CBF/CBF_TRAIN')
-    n = matrix.shape[0]
+    matrix, label = slice_data('./test_data', 3)
     print(matrix)
+    print(label)
